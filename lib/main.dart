1cbfbef0d447150d0756cee2346a31cee2595fbe
aarti <ragellaaarti@gmail.com>
2023-04-29 12:20:07 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: CountdownTimerList(),
@@ -32,90 +34,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
 
 class TimerItem {
   int timeInSeconds = 0;
@@ -144,56 +62,97 @@ class _CountdownTimerListState extends State<CountdownTimerList> {
         itemBuilder: (BuildContext context, int index) {
           TimerItem timerx = timers[index];
 
-          return Card(
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Enter time in seconds',
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        timerx.timeInSeconds = int.tryParse(value) ?? 0;
-                      });
-                    },
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 90,
+              width: 300,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: SizedBox(
+                          height: 50,
+                          child: Container(
+                            decoration: BoxDecoration(
+                               border: Border.all(color: Colors.black)
+                                  ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Enter seconds',
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  timerx.timeInSeconds = int.tryParse(value) ?? 0;
+                                  
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Container(
+                        height: 50,
+                            decoration: BoxDecoration(
+                               border: Border.all(color: Colors.black)
+                                  ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            _formatDuration(Duration(seconds: timerx.timeInSeconds)),textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      SizedBox(
+                        height: 50,
+                        width: 80,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:MaterialStateProperty.all<Color>(Colors.white),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            side: BorderSide(color: Colors.black)
+                          )
+                        )
+                      ),
+                          onPressed: () {
+                            setState(() {
+                              timerx.isRunning = !timerx.isRunning;
+                              if (timerx.isRunning) {
+                                timerx.timer = Timer.periodic(Duration(seconds: 1), (timer) {
+                                  setState(() {
+                                    if (timerx.timeInSeconds > 0) {
+                                      timerx.timeInSeconds--;
+                                    } else {
+                                      timerx.isRunning = false;
+                                      timerx.isResume = false;
+                                      timerx.timer?.cancel();
+                                    }
+                                  });
+                                });
+                              } else {
+                                timerx.timer?.cancel();
+                                timerx.isResume = true;
+                              }
+                            });
+                          },
+                          child: Text(timerx.isRunning ? 'Pause' :
+                          timerx.isResume ? 'Resume':"Start",style: TextStyle(color: Colors.black),),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(width: 8),
-                Text(
-                  _formatDuration(Duration(seconds: timerx.timeInSeconds)),
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(width: 8),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      timerx.isRunning = !timerx.isRunning;
-                      if (timerx.isRunning) {
-                        timerx.timer = Timer.periodic(Duration(seconds: 1), (timer) {
-                          setState(() {
-                            if (timerx.timeInSeconds > 0) {
-                              timerx.timeInSeconds--;
-                            } else {
-                              timerx.isRunning = false;
-                              timerx.isResume = false;
-                              timerx.timer?.cancel();
-                            }
-                          });
-                        });
-                      } else {
-                        timerx.timer?.cancel();
-                        timerx.isResume = true;
-                      }
-                    });
-                  },
-                  child: Text(timerx.isRunning ? 'Pause' :
-                  timerx.isResume ? 'Resume':"Start"),
-                ),
-              ],
+              ),
             ),
           );
         },
@@ -212,4 +171,6 @@ class _CountdownTimerListState extends State<CountdownTimerList> {
   String _formatDuration(Duration duration) {
     return '${duration.inHours.toString().padLeft(2, '0')}:${(duration.inMinutes % 60).toString().padLeft(2, '0')}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
   }
+
+  
 }
